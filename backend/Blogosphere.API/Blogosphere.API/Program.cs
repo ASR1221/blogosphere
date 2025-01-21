8f46782343bace4d7f5fc2c4ff0c2e49d2e7083b
Abdullah Salah <asr12211@outlook.com>
2025-01-21 21:17:17 +0300

    EDIT(program.cs): allow swagger in production

--- backend/Blogosphere.API/Blogosphere.API/Program.cs
+++ backend/Blogosphere.API/Blogosphere.API/Program.cs
@@ -125,11 +125,11 @@ builder.Services.AddScoped<IFileUploadService, FileUploadService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
 	app.UseSwagger();
 	app.UseSwaggerUI();
-}
+// }
 
 app.UseHsts();
 app.UseHttpsRedirection();
